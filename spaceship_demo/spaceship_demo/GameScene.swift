7260bb69dff13c27a267c3e58b1a93308f0feb75
Gustavo Garcia <garciatavo@live.com>
2019-10-14 20:55:45 -0400

    add the animation to the spaceship

--- spaceship_demo/spaceship_demo/GameScene.swift
+++ spaceship_demo/spaceship_demo/GameScene.swift
@@ -17,14 +17,16 @@ class GameScene: SKScene {
         
         backgroundColor = SKColor.black
         
-        spaceship.position = CGPoint(x: self.size.width * 0.15, y: self.size.height * 0.15)
+        spaceship.position = CGPoint(x: self.size.width * 0.2, y: self.size.height * 0.15)
         spaceship.setScale(0.1)
         spaceship.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         spaceship.zPosition = 5
         spaceship.name = "spaceship"
         addChild(spaceship)
         
-   
+        // Run animation
+        animateSpaceship()
+                
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -33,5 +35,19 @@ class GameScene: SKScene {
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    // Animation
+    func animateSpaceship(){
+        // move to the right of the screen
+        let rightMovement = SKAction.moveTo(x: self.size.width * 0.8, duration: 2.5)
+        
+        // move to the right of the screen
+        let leftMovement = SKAction.moveTo(x: self.size.width * 0.2 , duration: 2.5)
+        
+        let sequence = SKAction.sequence([rightMovement, leftMovement])
+        let animation = SKAction.repeatForever(sequence)
+        
+        spaceship.run(animation)
     }
 }
